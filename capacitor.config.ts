ac5fe2a6d5a048bc70ffd570fa3dccb4ded5eb6d
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-05-06 10:07:30 -0500

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'issue-53571',
+  webDir: 'www',
+};
+
+export default config;
