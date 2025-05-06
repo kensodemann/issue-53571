04e0018d4d6e5f270cc9cacb69e7da4e7dffde35
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-05-06 12:34:54 -0500

    manipulate the status bar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'issue-53571',
   webDir: 'www',
+  android: {
+    adjustMarginsForEdgeToEdge: 'auto',
+  },
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+    },
+  },
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
@@ -7,5 +8,20 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    StatusBar.setStyle({ style: Style.Default });
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    this.handleDarkModeChange(darkModeMediaQuery);
+    darkModeMediaQuery.addEventListener('change', this.handleDarkModeChange);
+  }
+
+  private async handleDarkModeChange(event: { matches: boolean }) {
+    if (event.matches) {
+      console.log('Dark Mode');
+      await StatusBar.setStyle({ style: Style.Dark });
+    } else {
+      console.log('Light Mode');
+      await StatusBar.setStyle({ style: Style.Light });
+    }
+  }
 }
